b9a01a0ec66c73673a5996ede46a09cfb87da029
Shwetabh <shwetabhbhu@gmail.com>
2025-07-27 20:33:32 +0530

    added the files for the first time

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    model_input = [
+        data['satisfaction_level'],
+        data['last_evaluation'],
+        data['number_project'],
+        data['average_montly_hours'],
+        data['time_spend_company'],
+        data['Work_accident'],
+        data['promotion_last_5years'],
+        data['low'],
+        data['medium']
+    ]
+    
+    model = joblib.load('model.pkl')
+    prediction = model.predict([model_input])[0]
+    
+    return jsonify({'prediction': int(prediction)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
